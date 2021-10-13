a2165322985a5b7874be01d2788e36433d48693c
Hleb Brazhynski <hleb200217@gmail.com>
2021-10-13 06:39:01 -0400

    functionsandarrays

---
+++ app.js
@@ -0,0 +1,29 @@
+function addTwo(param1, param2){
+    console.log( "The Sum is = " + param1, param2)
+}
+
+addTwo(15+10)
+
+
+function difTwo(param1, param2){
+    console.log( "The Difference is = " + param1, param2)
+}
+
+difTwo(15-10)
+
+function divideTwo(param1, param2){
+    console.log( "The Division is = " + param1, param2)
+}
+
+difTwo(15/10)
+
+function multiplyTwo(param1, param2){
+    console.log( "The Multiplication is = " + param1, param2)
+}
+
+difTwo(15*10)
+
+
+
+
+
